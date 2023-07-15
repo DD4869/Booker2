57135254a24caf09d846fd2c1302d5b2be00a37a
DD4869 <110597693+DD4869@users.noreply.github.com>
2023-07-15 06:48:39 +0000

    Final modify

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,7 +30,8 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    unless @book.user == current_user
+    @user = @book.user
+    unless @user == current_user
       redirect_to books_path
     end
   end
